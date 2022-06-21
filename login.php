50e32f8cd2353b10378b738022fd95589088e9ca
WebgemHimanshu <101526082+WebgemHimanshu@users.noreply.github.com>
2022-06-22 00:04:44 +0530

    Update
    Login files with validation code & DB function and connection file is done.

--- login.php
+++ login.php
@@ -23,5 +23,31 @@
       <div class="pass-txt"><a href="#">Forgot password?</a></div>
       <input type="submit" value="Submit">
     </form>
+
+    <?php
+                $fullUrl= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+                if(strpos($fullUrl,"admin_un-authorize-user")==true) {
+                    echo "<div class='alert alert-primary' role='alert'>
+                                Unauthorize Admin Access
+                            </div>";
+                }
+                if(strpos($fullUrl,"admin_username-empty")==true) {
+                    echo "<div class='alert alert-primary' role='alert'>
+                                Admin Username is empty
+                            </div>";
+                }
+                if(strpos($fullUrl,"admin_password-empty")==true) {
+                    echo "<div class='alert alert-primary' role='alert'>
+                                Admin Password is empty
+                            </div>";
+                }
+
+                if(strpos($fullUrl,"admin_invalid_password")==true) {
+                    echo "<div class='alert alert-primary' role='alert'>
+                                Invalid Admin Password
+                            </div>";
+                }
+                ?>
+
 </body>
 </html>
